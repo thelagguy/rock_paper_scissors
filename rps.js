8ac952fa42a331eb0aa8ec032a5cd2e0fda7069e
Alex <155367351+thelagguy@users.noreply.github.com>
2025-08-26 13:08:11 +0200

    Add files via upload
    main logic of rpc

--- rps.js
+++ rps.js
@@ -1,1 +1,28 @@
-console.log(`hello world`);
+function computer_choice(){
+    let num =Math.random ;
+    let choice ='';
+    let inpt_s =prompt(`Rock, Paper or Scissors?`);
+    inpt_s =inpt_s.toUpperCase(inpt_s) ;
+    console.log(inpt_s);
+
+    if (!(inpt_s ==="ROCK" ||inpt_s === "PAPER" ||inpt_s === "SCISSORS")){
+        console.log("neither rock,paper, or scissors");
+        stop;
+    }
+    switch (num){ //rock
+        case 0 || 0.33: choice =`Rock`;
+        break;
+
+        case 0.34 || 0.66: choice =`Scissors` //scissors
+            break;
+        
+        case 0.67 || 0.99: choice =`Paper` //paper
+            break ;
+
+    }
+    choice =choice.toUpperCase;
+    
+
+
+}
+computer_choice();
